a89fdce2c0bcb9b22f03f6ce6448b1edd5f55cb4
Sam Pepper <sammy-pep@hotmail.co.uk>
2018-11-23 17:32:47 +0000

    Initial commit.

---
+++ ConnectFourOnlineServer.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = 3000;
+
+app.get("/ConnectFourOnlineClient.js", function(req, res) {
+    res.sendFile(__dirname + "/ConnectFourOnlineClient.js");
+});
+
+app.get("/ConnectFourOnlineClient.css", function(req, res) {
+    res.sendFile(__dirname + "/ConnectFourOnlineClient.css");
+});
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/ConnectFourOnlineClient.html");
+});
+
+io.on("connection", function(socket){
+    console.log("A user has connected.");
+
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(port, function(){
+  console.log("Connect Four Online Server is listening on port: " + port);
+});
